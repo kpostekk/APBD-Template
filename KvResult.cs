0f6bc14fb3743c601c2529e012d9b8b9b078ac87
Krystian Postek <krystian@postek.eu>
2023-04-18 16:18:05 +0200

    feat: create KvResult entity

---
+++ KvResult.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp_Template.DTOs;
+
+public class KvResult
+{
+    [Required]
+    public string Key { get; init; }
+    [Required]
+    public string Value { get; init; }
+    [Required]
+    public DateTime Created { get; init; }
+}
