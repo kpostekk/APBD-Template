2974054d5ed46e6a9f4e24b514389ea6c8a544d7
Krystian Postek <krystian@postek.eu>
2023-04-18 14:58:54 +0200

    feat!: add db service, include swagger

--- Program.cs
+++ Program.cs
@@ -1,6 +1,25 @@
+using WebApp_Template.Services;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Add DB service
+builder.Services.AddHostedService<DatabaseService>();
+builder.Services.AddSingleton<DatabaseService>();
+
+// Add controllers
+builder.Services.AddControllers();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
 
 app.Run();
---
+++ Services/DatabaseService.cs
@@ -0,0 +1,28 @@
+﻿using System.Data.SqlClient;
+
+namespace WebApp_Template.Services;
+
+public class DatabaseService : IHostedService
+{
+    public readonly SqlConnection Connection;
+    private readonly ILogger<DatabaseService> _logger;
+    
+    public DatabaseService(IConfiguration configuration, ILogger<DatabaseService> logger)
+    {
+        _logger = logger;
+        Connection = new SqlConnection(configuration.GetConnectionString("Database"));
+        _logger.LogDebug("Created DatabaseService!");
+    }
+
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        await Connection.OpenAsync(cancellationToken);
+        _logger.LogDebug("Connected!");
+    }
+
+    public async Task StopAsync(CancellationToken cancellationToken)
+    {
+        await Connection.CloseAsync();
+        _logger.LogDebug("Closed!");
+    }
+}
